8d7e13560b294b3486d2e829b73436c152634d76
dan1510123 <dan1510123@gmail.com>
2020-05-13 20:40:34 -0400

    Added masking for categories of collideable. Also updated the player sprite

---
+++ FlappyGrad.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/Bitmasks.swift
@@ -0,0 +1,6 @@
+public struct Bitmask {
+    static let player : UInt32 = 1 << 1
+    static let pipe : UInt32 = 1 << 2
+    static let failSlot : UInt32 = 1 << 3
+    static let successSlot : UInt32 = 1 << 4
+}
--- FlappyGrad.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/GameScene.swift
+++ FlappyGrad.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/GameScene.swift
@@ -4,29 +4,62 @@ import SpriteKit
 public class GameScene : SKScene, SKPhysicsContactDelegate {
     /* Global fields */
     var player : SKSpriteNode!
+    
     var pipes = [SKSpriteNode]()
-    var gameStarted : Bool = false
-    var textField : UITextField = UITextField()
+    let pipeSpeed : CGFloat = 1
+    
+    var zoomScore = 0
+    var currentTitle = "Freshman"
+    var scoreTextField : UITextView = UITextView()
+    var centerTextField : UITextView = UITextView()
+    var degreeLevelTextField : UITextView = UITextView()
+    var gameIsActive : SKNode!
     
     public override func didMove(to view: SKView) {
+        // Set up gameIsActive node
+        gameIsActive = SKNode()
+        gameIsActive.speed = 0
+        self.addChild(gameIsActive)
+        
         // Set up the Physics of the Scene
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsBody?.friction = 0.0
         // Contact (collisions) will be handled by this class
         physicsWorld.contactDelegate = self 
         
-        setupTextField() // Set up UITextField
+        initDegreeLevelTextField()
+        setupInitialTextField() // Set up UITextField
+        updateScoreTextField()
         setBackground() // Set background
         createPlayer() // Create player
     }
     
-    private func setupTextField() {
+    private func setupInitialTextField() {
         let textFieldWidth : CGFloat = 500
         let textFieldHeight : CGFloat = 150
-        textField.frame = CGRect(x: frame.midX - 100, y: frame.midY - CGFloat(textFieldHeight / 2) - 203, width: textFieldWidth, height: textFieldHeight)
-        textField.text = "Hello"
-        textField.textAlignment = .center
-        textField.backgroundColor = UIColor.red
+        centerTextField.frame = CGRect(x: frame.midX - 100, y: frame.midY - CGFloat(textFieldHeight / 2) - 203, width: textFieldWidth, height: textFieldHeight)
+        centerTextField.textAlignment = .center
+        centerTextField.backgroundColor = UIColor.red
+        centerTextField.text = "Hello"
+    }
+    
+    private func initDegreeLevelTextField() {
+        let textFieldWidth : CGFloat = 300
+        let textFieldHeight : CGFloat = 650
+        degreeLevelTextField.frame = CGRect(x: frame.midX + 500, y: frame.midY - 275, width: textFieldWidth, height: textFieldHeight)
+        degreeLevelTextField.backgroundColor = UIColor.brown
+        degreeLevelTextField.font = UIFont.boldSystemFont(ofSize: 25)
+        degreeLevelTextField.text = "\nLEVEL GUIDE\n\n" + "Undergrad Senior   0\n\n" + "B.S. Degree              15\n\n" + "M.S. Degree            30\n\n" + "Ph.D.                           50\n\n" + "M.D. Ph.D.              100\n\n" + "Zoom Lord            200"
+    }
+    
+    private func updateScoreTextField() {
+        let textFieldWidth : CGFloat = 360
+        let textFieldHeight : CGFloat = 150
+        scoreTextField.frame = CGRect(x: frame.midX - 470, y: frame.midY - CGFloat(textFieldHeight / 2) - 203, width: textFieldWidth, height: textFieldHeight)
+        scoreTextField.textAlignment = .center
+        scoreTextField.backgroundColor = UIColor.blue
+        scoreTextField.font = UIFont.boldSystemFont(ofSize: 35)
+        scoreTextField.text = String(1000) + " Zoom Credits\n" + currentTitle
     }
     
     private func setBackground() {
@@ -37,17 +70,20 @@ public class GameScene : SKScene, SKPhysicsContactDelegate {
         bg.position = CGPoint(x: frame.midX, y: frame.midY)
         addChild(bg)
     }
+    
     private func createPlayer() {
-        let birdTexture1 = SKTexture(image: #imageLiteral(resourceName: "bird.png"))
-        let birdTexture2 = SKTexture(image: #imageLiteral(resourceName: "bird2.png"))
+        let birdTexture1 = SKTexture(image: #imageLiteral(resourceName: "bird1new.png"))
+        let birdTexture2 = SKTexture(image: #imageLiteral(resourceName: "bird2new.png"))
         
         player = SKSpriteNode(texture: birdTexture1)
         player.setScale(2)
         player.zPosition = 0
-        player.position = CGPoint(x: frame.midX - 200, y: frame.midY)
+        player.position = CGPoint(x: frame.midX - 190, y: frame.midY)
         addChild(player)
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.height / 2.0)
         player.physicsBody?.affectedByGravity = false;
+        player.physicsBody?.categoryBitMask = Bitmask.player
+        player.physicsBody?.contactTestBitMask = Bitmask.pipe | Bitmask.failSlot | Bitmask.successSlot
         // Set flap animation for player
         let flapAnimation = SKAction.animate(with: [birdTexture1, birdTexture2], timePerFrame: 0.4)
         let flapAction = SKAction.repeatForever(flapAnimation)
@@ -61,16 +97,35 @@ public class GameScene : SKScene, SKPhysicsContactDelegate {
     }
     
     public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        gameStarted = true
+        gameIsActive.speed = 1
         player.physicsBody?.affectedByGravity = true
-        for _ in touches { // do we need all touches?
-            player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 15))
+        player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 20))
+    }
+    
+    public func didBegin(_ contact: SKPhysicsContact) {
+        if gameIsActive.speed > 0 {
+            if (contact.bodyA.categoryBitMask & Bitmask.successSlot) == Bitmask.successSlot || (contact.bodyB.categoryBitMask & Bitmask.successSlot) == Bitmask.successSlot {
+                // Handle a collision with the successSlot (increment score)
+                zoomScore += 1
+                
+            }
+            else {
+                
+            }
         }
     }
     
-    public func getTextField () -> UITextField {
-        return textField;
+    public func getScoreTextField() -> UITextView {
+        return scoreTextField
+    }
+    
+    public func getCenterTextField() -> UITextView {
+        return centerTextField
+    }
+    
+    public func getDegreeLevelTextField() -> UITextView {
+        return degreeLevelTextField
     }
 }
 
