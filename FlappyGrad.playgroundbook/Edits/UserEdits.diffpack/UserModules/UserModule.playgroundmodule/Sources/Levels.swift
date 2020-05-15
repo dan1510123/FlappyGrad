31d24cf951fc909e56a58e81fff15575a95aece7
dan1510123 <dan1510123@gmail.com>
2020-05-15 17:00:17 -0400

    Removed unused images and updated levels with correct scoreRequired

--- FlappyGrad.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/Levels.swift
+++ FlappyGrad.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/Levels.swift
@@ -9,8 +9,8 @@ public class Level {
 public struct Levels {
     static let ugs = Level(name: "Undergrad Senior", scoreRequired: 0)
     static let bsd = Level(name: "B.S. Degree", scoreRequired: 1)
-    static let msd = Level(name: "M.S. Degree", scoreRequired: 2)
-    static let phd = Level(name: "Ph.D.", scoreRequired: 3)
-    static let mdphd = Level(name: "M.D. Ph.D", scoreRequired: 4)
-    static let zl = Level(name: "* Zoom Lord *", scoreRequired: 5)
+    static let msd = Level(name: "M.S. Degree", scoreRequired: 30)
+    static let phd = Level(name: "Ph.D.", scoreRequired: 50)
+    static let mdphd = Level(name: "M.D. Ph.D", scoreRequired: 80)
+    static let zl = Level(name: "* Zoom Lord *", scoreRequired: 100)
 }
